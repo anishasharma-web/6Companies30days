f0e6092add202486ed528a978988551006450e6d
anishasharma-web <56109423+anishasharma-web@users.noreply.github.com>
2022-01-05 16:45:16 +0530

    Create a. Max_10numbers.cpp

---
+++ Milestone1 - Goldman Sachs/a. Max_10numbers.cpp
@@ -0,0 +1,27 @@
+/*
+Problem Link: Not Given
+Title: Find max 10 numbers in a list having 10M entries.
+Difficulty: Medium
+Author: Anisha Sharma
+Language: C++
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+public:
+    void maximum(int A[],int n,int k)
+    {
+        vector<int> ans;
+        priority_queue<int> p;
+        for(int i=0;i<n;i++)
+            p.push(A[i]);
+        for(int i=0;i<k;i++)
+        {
+            ans.push_back(p.top());
+            p.pop();
+        }
+        for(auto i:ans)
+            cout<<i<<" ";
+    }
+};
