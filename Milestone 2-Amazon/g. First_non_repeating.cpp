e0face5c286ba81376c9167bbf9221a159227653
anishasharma-web <56109423+anishasharma-web@users.noreply.github.com>
2022-01-10 23:10:53 +0530

    Create g. First_non_repeating.cpp

---
+++ Milestone 2-Amazon/g. First_non_repeating.cpp
@@ -0,0 +1,52 @@
+/*
+Problem Link: https://practice.geeksforgeeks.org/problems/first-non-repeating-character-in-a-stream1216/1
+Title: First non-repeating character in a stream 
+Difficulty: Medium
+Author: Anisha Sharma
+Language: C++
+*/
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+	public:
+		string FirstNonRepeating(string A){
+		    // Code here
+		      queue<int> q;
+		    string temp="";
+		    int arr[26]={0};
+		    for(int i=0;i<A.size();i++){
+		        arr[A[i]-'a']++;
+		        if(arr[A[i]-'a']==1){
+		            q.push(A[i]);
+		        }
+		        while(!q.empty() && arr[q.front()-'a']!=1){
+		            q.pop();
+		        }
+		        if(q.empty()){
+		            A[i]='#';
+		        }else{
+		            A[i]=q.front();
+		        }
+		    }
+		    return A;
+		}
+
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string A;
+		cin >> A;
+		Solution obj;
+		string ans = obj.FirstNonRepeating(A);
+		cout << ans << "\n";
+	}
+	return 0;
+}  // } Driver Code Ends
