00df9727e6f155f88989c6426ef457a1f42a014b
anishasharma-web <56109423+anishasharma-web@users.noreply.github.com>
2022-01-10 23:18:52 +0530

    Create j. Nuts_and_bolts.cpp

---
+++ Milestone 2-Amazon/j. Nuts_and_bolts.cpp
@@ -0,0 +1,49 @@
+/*
+Problem Link: https://practice.geeksforgeeks.org/problems/nuts-and-bolts-problem0431/1#
+Title: Nuts and Bolts
+Difficulty: Hard
+Author: Anisha Sharma
+Language: C++
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:	
+
+	void matchPairs(char nuts[], char bolts[], int n) {
+	    // code here
+	    sort(nuts,nuts+n);
+	    sort(bolts,bolts+n);
+	}
+
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        char nuts[n], bolts[n];
+        for (int i = 0; i < n; i++) {
+            cin >> nuts[i];
+        }
+        for (int i = 0; i < n; i++) {
+            cin >> bolts[i];
+        }
+        Solution ob;
+        ob.matchPairs(nuts, bolts, n);
+        for (int i = 0; i < n; i++) {
+            cout << nuts[i] << " ";
+        }
+        cout << "\n";
+        for (int i = 0; i < n; i++) {
+            cout << bolts[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+  
